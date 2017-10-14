e658a829ecf0f107ddbfac125543c28542c37657
CHLLHC <chl41993@gmail.com>
2017-10-13 22:17:35 -0700

    He

--- Source.c
+++ Source.c
@@ -37,97 +37,6 @@ void sijk(double a[], double b[], double c[], int n) {
 	printf("Simple ijk, n=%d, Time:%ld seconds and %ld nanoseconds.\n", n, diff.tv_sec, diff.tv_nsec);
 }
 
-void sjik(double a[], double b[], double c[], int n) {
-	struct timespec begin, end, diff;
-	double time;
-	int i, j, k;
-
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-	for (j = 0; j < n; ++j)
-		for (i = 0; i < n; ++i) {
-			register double r = c[i*n + j];
-			for (k = 0; k < n; ++k) {
-				r += a[i*n + k] * b[k*n + j];
-			}
-			c[i*n + j] = r;
-		}
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = HDdiff(begin, end);
-	printf("Simple jik, n=%d, Time:%ld seconds and %ld nanoseconds.\n", n, diff.tv_sec, diff.tv_nsec);
-}
-
-void sikj(double a[], double b[], double c[], int n) {
-	struct timespec begin, end, diff;
-	double time;
-	int i, j, k;
-
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-	for (i = 0; i < n; ++i)
-		for (k = 0; k < n; ++k) {
-			register double r = a[i*n + k];
-			for (j = 0; j < n; ++j) {
-				c[i*n + j] += r * b[k*n + j];
-			}
-		}
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = HDdiff(begin, end);
-	printf("Simple ikj, n=%d, Time:%ld seconds and %ld nanoseconds.\n", n, diff.tv_sec, diff.tv_nsec);
-}
-
-void skij(double a[], double b[], double c[], int n) {
-	struct timespec begin, end, diff;
-	double time;
-	int i, j, k;
-
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-	for (k = 0; k < n; ++k)
-		for (i = 0; i < n; ++i) {
-			register double r = a[i*n + k];
-			for (j = 0; j < n; ++j) {
-				c[i*n + j] += r * b[k*n + j];
-			}
-		}
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = HDdiff(begin, end);
-	printf("Simple kij, n=%d, Time:%ld seconds and %ld nanoseconds.\n", n, diff.tv_sec, diff.tv_nsec);
-}
-
-void sjki(double a[], double b[], double c[], int n) {
-	struct timespec begin, end, diff;
-	double time;
-	int i, j, k;
-
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-	for (j = 0; j < n; ++j)
-		for (k = 0; k < n; ++k) {
-			register double r = b[k*n + j];
-			for (i = 0; i < n; ++i) {
-				c[i*n + j] += a[i*n + k] * r;
-			}
-		}
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = HDdiff(begin, end);
-	printf("Simple jki, n=%d, Time:%ld seconds and %ld nanoseconds.\n", n, diff.tv_sec, diff.tv_nsec);
-}
-
-void skji(double a[], double b[], double c[], int n) {
-	struct timespec begin, end, diff;
-	double time;
-	int i, j, k;
-
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-	for (k = 0; k < n; ++k)
-		for (j = 0; j < n; ++j) {
-			register double r = b[k*n + j];
-			for (i = 0; i < n; ++i) {
-				c[i*n + j] += a[i*n + k] * r;
-			}
-		}
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = HDdiff(begin, end);
-	printf("Simple kji, n=%d, Time:%ld seconds and %ld nanoseconds.\n", n, diff.tv_sec, diff.tv_nsec);
-}
-
 void bijk(double a[], double b[], double c[], int n, int B) {
 	struct timespec begin, end, diff;
 	double time;
@@ -137,137 +46,34 @@ void bijk(double a[], double b[], double c[], int n, int B) {
 
 	for (i = 0; i < n; i += B)
 		for (j = 0; j < n; j += B)
-			for (k = 0; k < n; k += B)
+			for (k = 0; k < n; k += B) {
 				/* B x B mini matrix multiplications */
-				for (i1 = i; i1 < i + B; i1++)
-					for (j1 = j; j1 < j + B; j1++) {
-						register double r = c[i1*n + j1];
-						for (k1 = k; k1 < k + B; k1++)
-							r += a[i1*n + k1] * b[k1*n + j1];
-						c[i1*n + j1] = r;
+				for (i1 = i; i1 < i + B; i1 += 2)
+					for (j1 = j; j1 < j + B; j1 += 2) {
+						register int in = i1*n, i1n = in + n;
+						register double cij = c[in + j1], ci1j = c[i1n + j1],
+							cij1 = c[in + (j1 + 1)], ci1j1 = c[i1n + (j1 + 1)];
+						for (k1 = k; k1 < k + B; k1 += 2) {
+							register double aik = a[in + k1], aik1 = a[in + k1 + 1],
+								ai1k = a[i1n + k1], ai1k1 = a[i1n + k1 + 1];
+							register int kn = k1*n, k1n = kn + n;
+							register double bkj = b[kn + j1], bk1j = b[k1n + j1],
+								bkj1 = b[kn + (j1 + 1)], bk1j1 = b[k1n + (j1 + 1)];
+							cij = aik * bkj + aik1 * bk1j + cij;
+							ci1j = ai1k * bkj + ai1k1 * bk1j + ci1j;
+							cij1 = aik * bkj1 + aik1 * bk1j1 + cij1;
+							ci1j1 = ai1k * bkj1 + ai1k1 * bk1j1 + ci1j1;
+						}
+						c[in + j1] = cij;
+						c[i1n + j1] = ci1j;
+						c[in + (j1 + 1)] = cij1;
+						c[i1n + (j1 + 1)] = ci1j1;
 					}
+			}
 
 	clock_gettime(CLOCK_MONOTONIC, &end);
 	diff = HDdiff(begin, end);
 	printf("Blocked ijk, n=%d, B=%d, Time:%ld seconds and %ld nanoseconds.\n", n, B, diff.tv_sec, diff.tv_nsec);
-}
-
-void bjik(double a[], double b[], double c[], int n, int B) {
-	struct timespec begin, end, diff;
-	double time;
-	int i, j, k, i1, j1, k1;
-
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-
-	for (j = 0; j < n; j += B)
-		for (i = 0; i < n; i += B)
-			for (k = 0; k < n; k += B)
-				/* B x B mini matrix multiplications */
-				for (j1 = j; j1 < j + B; j1++)
-					for (i1 = i; i1 < i + B; i1++) {
-						register double r = c[i1*n + j1];
-						for (k1 = k; k1 < k + B; k1++)
-							r += a[i1*n + k1] * b[k1*n + j1];
-						c[i1*n + j1] = r;
-					}
-
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = HDdiff(begin, end);
-	printf("Blocked jik, n=%d, B=%d, Time:%ld seconds and %ld nanoseconds.\n", n, B, diff.tv_sec, diff.tv_nsec);
-}
-
-void bikj(double a[], double b[], double c[], int n, int B) {
-	struct timespec begin, end, diff;
-	double time;
-	int i, j, k, i1, j1, k1;
-
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-
-	for (i = 0; i < n; i += B)
-		for (k = 0; k < n; k += B)
-			for (j = 0; j < n; j += B)
-				/* B x B mini matrix multiplications */
-				for (i1 = i; i1 < i + B; i1++)
-					for (k1 = k; k1 < k + B; k1++) {
-						register double r = a[i1*n + k1];
-						for (j1 = j; j1 < j + B; j1++)
-							c[i1*n + j1] += r * b[k1*n + j1];
-					}
-
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = HDdiff(begin, end);
-	printf("Blocked ikj, n=%d, B=%d, Time:%ld seconds and %ld nanoseconds.\n", n, B, diff.tv_sec, diff.tv_nsec);
-}
-
-void bkij(double a[], double b[], double c[], int n, int B) {
-	struct timespec begin, end, diff;
-	double time;
-	int i, j, k, i1, j1, k1;
-
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-
-	for (k = 0; k < n; k += B)
-		for (i = 0; i < n; i += B)
-			for (j = 0; j < n; j += B)
-				/* B x B mini matrix multiplications */
-				for (k1 = k; k1 < k + B; k1++)
-					for (i1 = i; i1 < i + B; i1++) {
-						register double r = a[i1*n + k1];
-						for (j1 = j; j1 < j + B; j1++)
-							c[i1*n + j1] += r * b[k1*n + j1];
-					}
-
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = HDdiff(begin, end);
-	printf("Blocked kij, n=%d, B=%d, Time:%ld seconds and %ld nanoseconds.\n", n, B, diff.tv_sec, diff.tv_nsec);
-}
-
-void bjki(double a[], double b[], double c[], int n, int B) {
-	struct timespec begin, end, diff;
-	double time;
-	int i, j, k, i1, j1, k1;
-
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-
-
-	for (j = 0; j < n; j += B)
-		for (k = 0; k < n; k += B)
-			for (i = 0; i < n; i += B)
-				/* B x B mini matrix multiplications */
-				for (j1 = j; j1 < j + B; j1++)
-					for (k1 = k; k1 < k + B; k1++) {
-						register double r = b[k1*n + j1];
-						for (i1 = i; i1 < i + B; i1++)
-							c[i1*n + j1] += a[i1*n + k1] * r;
-					}
-
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = HDdiff(begin, end);
-	printf("Blocked jki, n=%d, B=%d, Time:%ld seconds and %ld nanoseconds.\n", n, B, diff.tv_sec, diff.tv_nsec);
-}
-
-void bkji(double a[], double b[], double c[], int n, int B) {
-	struct timespec begin, end, diff;
-	double time;
-	int i, j, k, i1, j1, k1;
-
-	clock_gettime(CLOCK_MONOTONIC, &begin);
-
-
-	for (k = 0; k < n; k += B)
-		for (j = 0; j < n; j += B)
-			for (i = 0; i < n; i += B)
-				/* B x B mini matrix multiplications */
-				for (k1 = k; k1 < k + B; k1++)
-					for (j1 = j; j1 < j + B; j1++) {
-						register double r = b[k1*n + j1];
-						for (i1 = i; i1 < i + B; i1++)
-							c[i1*n + j1] += a[i1*n + k1] * r;
-					}
-
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	diff = HDdiff(begin, end);
-	printf("Blocked kji, n=%d, B=%d, Time:%ld seconds and %ld nanoseconds.\n", n, B, diff.tv_sec, diff.tv_nsec);
 }
 
 double CheckMaxDiff(double a[], double b[], int n) {
@@ -289,7 +95,6 @@ double CheckMaxDiff(double a[], double b[], int n) {
 
 int main(int argc, char* argv[]) {
 
-	//int blcokSize[6] = { 2,4,8,16,32,64 };
 	int i, j, n, B = 16;
 	double *a, *b, *c, *c1;
 	double maxDiff;
@@ -304,76 +109,14 @@ int main(int argc, char* argv[]) {
 		b[j] = (double)((rand() << 15) | rand()) / (double)rand();
 	}
 	memset(c, 0, sizeof(double)*n*n);
-	sijk(a, b, c, n);
+	//sijk(a, b, c, n);
 	printf("This is the reference result.\n");
-	memset(c1, 0, sizeof(double)*n*n);
-	sikj(a, b, c1, n);
-	CheckMaxDiff(c, c1, n);
-	memset(c1, 0, sizeof(double)*n*n);
-	sjik(a, b, c1, n);
-	CheckMaxDiff(c, c1, n);
-	memset(c1, 0, sizeof(double)*n*n);
-	sjki(a, b, c1, n);
-	CheckMaxDiff(c, c1, n);
-	memset(c1, 0, sizeof(double)*n*n);
-	skij(a, b, c1, n);
-	CheckMaxDiff(c, c1, n);
-	memset(c1, 0, sizeof(double)*n*n);
-	skji(a, b, c1, n);
-	CheckMaxDiff(c, c1, n);
 
 	//BLOCKED ALGORITHM
 	memset(c1, 0, sizeof(double)*n*n);
 	bijk(a, b, c1, n, B);
-	CheckMaxDiff(c, c1, n);
-	memset(c1, 0, sizeof(double)*n*n);
-	bikj(a, b, c1, n, B);
-	CheckMaxDiff(c, c1, n);
-	memset(c1, 0, sizeof(double)*n*n);
-	bjik(a, b, c1, n, B);
-	CheckMaxDiff(c, c1, n);
-	memset(c1, 0, sizeof(double)*n*n);
-	bjki(a, b, c1, n, B);
-	CheckMaxDiff(c, c1, n);
-	memset(c1, 0, sizeof(double)*n*n);
-	bkij(a, b, c1, n, B);
-	CheckMaxDiff(c, c1, n);
-	memset(c1, 0, sizeof(double)*n*n);
-	bkji(a, b, c1, n, B);
-	CheckMaxDiff(c, c1, n);
+	//CheckMaxDiff(c, c1, n);
 
-	/*
-	for (i = 0; i < 6; ++i) {
-		B = blcokSize[i];
-		memset(c1, 0, sizeof(double)*n*n);
-		bijk(a, b, c1, n, B);
-	}
-	for (i = 0; i < 6; ++i) {
-		B = blcokSize[i];
-		memset(c1, 0, sizeof(double)*n*n);
-		bikj(a, b, c1, n, B);
-	}
-	for (i = 0; i < 6; ++i) {
-		B = blcokSize[i];
-		memset(c1, 0, sizeof(double)*n*n);
-		bjik(a, b, c1, n, B);
-	}
-	for (i = 0; i < 6; ++i) {
-		B = blcokSize[i];
-		memset(c1, 0, sizeof(double)*n*n);
-		bjki(a, b, c1, n, B);
-	}
-	for (i = 0; i < 6; ++i) {
-		B = blcokSize[i];
-		memset(c1, 0, sizeof(double)*n*n);
-		bkij(a, b, c1, n, B);
-	}
-	for (i = 0; i < 6; ++i) {
-		B = blcokSize[i];
-		memset(c1, 0, sizeof(double)*n*n);
-		bkji(a, b, c1, n, B);
-	}
-	*/
 	free(a);
 	free(b);
 	free(c);
